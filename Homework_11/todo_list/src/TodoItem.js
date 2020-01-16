474f7b5f19059f5c7d377256f6bead9678d7ed82
araikgevorgian <arayik.gevorgyan@yandex.ru>
2020-01-16 15:36:11 +0400

    finished-before refactoring HomeWork_11: TODO LIST using React.js

--- Homework_11/todo_list/src/TodoItem.js
+++ Homework_11/todo_list/src/TodoItem.js
@@ -4,14 +4,23 @@ import './css/TodoItem.css';
 class TodoItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {checked: false, value: this.props.value};
+        this.state = {checked: false, value: this.props.value, isEmpty: !this.props.value};
     }
 
     handleCheck = () => {
         this.setState({checked: !this.state.checked, value: this.props.value})
     };
 
+    handleDelete = () => {
+        this.setState({checked: this.state.checked,
+            value: this.state.value,
+            isEmpty: !this.state.isEmpty});
+    };
+
     render() {
+        if(this.state.isEmpty){
+            return null;
+        }
         return (
                 <li className="todoItem" style={(this.state.checked ?
                     {background: "#303030"} : {})}>
@@ -19,6 +28,7 @@ class TodoItem extends React.Component {
                     <span id="spanText" style={(this.state.checked ?
                         {textDecoration: "line-through"} : {textDecoration: "none"})}>
                         {this.state.value}</span>
+                    <span id="spanDelete" onClick={this.handleDelete}>x</span>
                 </li>
         );
     }
